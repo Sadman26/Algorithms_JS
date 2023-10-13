5ac2932555f21e49abd7ab8beb7a3aec905a4f4a
Sadman26 <fareless.ranger@gmail.com>
2023-10-13 07:05:13 +0600

    Traverse it with while loop

--- Build_In_Func.js
+++ Build_In_Func.js
@@ -1,5 +1,7 @@
 let arr = [11, 4, 7, 21, 3, 4, 7, 88, 99, 1, 123];
 arr.sort((a, b) => a - b)
-for (let i = 0; i < arr.length; i++) {
+let i = 0;
+while (i < arr.length) {
     console.log(arr[i]);
+    i++;
 }
