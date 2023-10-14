411972bfea19718029b55dd0255dc8fe2f7d0662
Sadman26 <fareless.ranger@gmail.com>
2023-10-14 09:31:12 +0600

    Inheritence

--- OOP.js
+++ OOP.js
@@ -6,7 +6,17 @@ class Animal {
         console.log("This is a ", this.name)
     }
 }
+class Dog extends Animal {
+    constructor(name) {
+        super(name)
+    }
+    show() {
+        console.log("This is a ", this.name)
+    }
+}
 let obj = new Animal("Cat")
 let obj2 = new Animal("Dog")
 obj.show()
 obj2.show()
+let obj3 = new Dog("Dog")
+obj3.show()
