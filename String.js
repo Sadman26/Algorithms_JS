dbb9b0a9399e5cfa211d6b242116b47828efc1f0
Bm Shadman Sakib Mahee <fareless.ranger@gmail.com>
2023-10-26 16:36:17 +0600

    String Operations

--- String.js
+++ String.js
@@ -1,3 +1,12 @@
 let text = "I am deshi";
-let xx1 = text.substring(5, 11);
-console.log(xx1);
+let text = "Sadman Sakib Mahee"
+console.log(text.replace('Mahee', 'BOSS'))
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+console.log(text.split(' '));
+console.log(text.charAt(0));
+console.log(text.charCodeAt(0));
+console.log(text.codePointAt(0));
+console.log(text.endsWith('e'));
+console.log(text.search(0));
+console.log(text.includes('S', 0));
