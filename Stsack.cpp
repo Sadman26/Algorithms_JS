cd178108f14321273758a48055737a2c4dacf7df
Sadman26 <fareless.ranger@gmail.com>
2023-11-16 17:32:09 +0600

    stk

---
+++ Stsack.cpp
@@ -0,0 +1,33 @@
+// Assalamu Alaikum 💖
+// Bm.Shadman Sakib Mahee (Sadman26)
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    stack<int> stk;
+    stack<int> stk2;
+    int n;
+    int x;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        stk.push(x);
+    }
+    while (!stk.empty())
+    {
+        cout << "================================================"
+             << endl
+             << "Top: " << stk.top() << endl
+             << "Remaining- " << endl;
+        stk.pop();
+        stk2 = stk;
+        while (!stk2.empty())
+        {
+            cout << stk2.top() << " " << endl;
+            stk2.pop();
+        }
+    }
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+   // while (!stk2.empty())
+            // {
+            //     cout << stk2.top() << " " << endl;
+            //     stk2.pop();
+            // }
